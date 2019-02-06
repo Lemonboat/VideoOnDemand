f88f8ca4b5bfafdd4bcf26c387d9a9c50c9884eb
David Keiskander <keiskander@hotmail.com>
2019-02-06 10:06:41 +0100

    VideoOnDemand

---
+++ VideoOnDemand.Data/Data/Entities/Download.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VideoOnDemand.Data.Data.Entities
+{
+    public class Download
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(80), Required]
+        public string Title { get; set; }
+        [MaxLength(1024)]
+        public string Url { get; set; }
+        public Module Module { get; set; }
+        public int ModuleId { get; set; }
+
+        public Course Course { get; set; }
+        public int CourseId { get; set; }
+    }
+}
